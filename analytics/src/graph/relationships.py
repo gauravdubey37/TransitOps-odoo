33d3cb1349411a3e6910cb2d5c9791b6c5f6f31f
CodeWithDubeyji <dubeyj313@gmail.com>
2026-07-12 15:05:13 +0530

    feat: add missing relationships

--- analytics/src/graph/relationships.py
+++ analytics/src/graph/relationships.py
@@ -37,4 +37,40 @@ class RelationshipManager:
         """
         neo4j_client.execute_write(query, trip_id=trip_id, route_id=route_id)
 
+    @staticmethod
+    def create_vehicle_depot(vehicle_id: str, depot_id: str):
+        query = """
+        MATCH (v:Vehicle {vehicle_id: $vehicle_id})
+        MATCH (d:Depot {depot_id: $depot_id})
+        MERGE (v)-[rel:LOCATED_AT]->(d)
+        """
+        neo4j_client.execute_write(query, vehicle_id=vehicle_id, depot_id=depot_id)
+
+    @staticmethod
+    def create_driver_depot(driver_id: str, depot_id: str):
+        query = """
+        MATCH (dr:Driver {driver_id: $driver_id})
+        MATCH (d:Depot {depot_id: $depot_id})
+        MERGE (dr)-[rel:BASED_AT]->(d)
+        """
+        neo4j_client.execute_write(query, driver_id=driver_id, depot_id=depot_id)
+
+    @staticmethod
+    def create_trip_expense(trip_id: str, expense_id: str):
+        query = """
+        MATCH (t:Trip {trip_id: $trip_id})
+        MATCH (e:Expense {expense_id: $expense_id})
+        MERGE (t)-[rel:INCURRED]->(e)
+        """
+        neo4j_client.execute_write(query, trip_id=trip_id, expense_id=expense_id)
+
+    @staticmethod
+    def create_trip_fuel(trip_id: str, log_id: str):
+        query = """
+        MATCH (t:Trip {trip_id: $trip_id})
+        MATCH (f:FuelLog {log_id: $log_id})
+        MERGE (t)-[rel:CONSUMED]->(f)
+        """
+        neo4j_client.execute_write(query, trip_id=trip_id, log_id=log_id)
+
 relationship_manager = RelationshipManager()
