e3648964d214c13feacb9eec63ac33696d3bc5f8
CodeWithDubeyji <dubeyj313@gmail.com>
2026-07-12 15:08:28 +0530

    feat: implement missing graph status and sync APIs

--- analytics/src/main.py
+++ analytics/src/main.py
@@ -4,6 +4,7 @@ from src.api.kpi_api import router as kpi_router
 from src.api.recommendation_api import router as recommendation_router
 from src.api.nlp_api import router as nlp_router
 from src.api.voice_api import router as voice_router
+from src.api.graph_api import router as graph_router
 
 app = FastAPI(
     title="TransitOps Analytics API",
@@ -16,6 +17,7 @@ app.include_router(kpi_router)
 app.include_router(recommendation_router)
 app.include_router(nlp_router)
 app.include_router(voice_router)
+app.include_router(graph_router)
 
 @app.get("/")
 def read_root():
